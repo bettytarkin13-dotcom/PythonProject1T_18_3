132b361553d38ee991cc5b29efc0497c3406b846
bettytarkin13-dotcom <bettytarkin13@gmail.com>
2026-03-18 21:50:07 +0200

    #3

---
+++ targil4.py
@@ -0,0 +1,12 @@
+
+def nth_biggest(numbers,n):
+    unique_numbers = list(set(numbers))
+    unique_numbers.sort(reverse=True)
+
+    if n>len(unique_numbers)or n<=0:
+       return None
+
+    return unique_numbers[n-1]
+
+nums=[88,90,95,95,97,97,97,99,100]
+print(nth_biggest(nums,4))
